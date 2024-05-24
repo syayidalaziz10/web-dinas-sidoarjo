2099aed6930f11b4e26e1048d13e001d6f4cf466
syayidalaziz10 <shorashiro15@gmail.com>
2024-05-24 09:21:21 +0700

    [restyle footer web page] change file

--- template-4/views/footer.php
+++ template-4/views/footer.php
@@ -1,45 +1,102 @@
 <!-- Footer -->
-<div class="footer">
-   <img class="decoration-city" src="images/decoration-city.svg" alt="alternative">
-   <div class="container footer-content">
-      <div class="row">
-         <div class="col-lg-12">
-            <h3 class="mb-3">Dinas Komunikasi dan Informatika</h3>
-            <h5>Pellentesque aliquet mi eu tortor dictum, non imperdiet ante viverra. Phasellus eget enim orci ut pellentes troc</h5>
-            <div class="social-container mt-3">
-               <span class="fa-stack">
-                  <a href="#your-link">
-                     <i class="fas fa-circle fa-stack-2x"></i>
-                     <i class="fab fa-facebook-f fa-stack-1x"></i>
-                  </a>
-               </span>
-               <span class="fa-stack">
-                  <a href="#your-link">
-                     <i class="fas fa-circle fa-stack-2x"></i>
-                     <i class="fab fa-twitter fa-stack-1x"></i>
-                  </a>
-               </span>
-               <span class="fa-stack">
-                  <a href="#your-link">
-                     <i class="fas fa-circle fa-stack-2x"></i>
-                     <i class="fab fa-pinterest-p fa-stack-1x"></i>
-                  </a>
-               </span>
-               <span class="fa-stack">
-                  <a href="#your-link">
-                     <i class="fas fa-circle fa-stack-2x"></i>
-                     <i class="fab fa-instagram fa-stack-1x"></i>
-                  </a>
-               </span>
-               <span class="fa-stack">
-                  <a href="#your-link">
-                     <i class="fas fa-circle fa-stack-2x"></i>
-                     <i class="fab fa-youtube fa-stack-1x"></i>
-                  </a>
-               </span>
-            </div>
-         </div> 
+      <!-- ============================================-->
+      <!-- <section> begin ============================-->
+<section class="bg-info pt-0 pb-0">
+
+   <div class="container">
+      <div class="row justify-content-sm-between py-6">
+      <div class="col-auto mb-2">
+         <div class="d-flex">
+            <svg class="bi bi-check-circle" xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#ffffff" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
+            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"></path>
+            </svg>
+            <p class="mb-0 text-light ms-2">30 day money back guarantee</p>
+         </div>
       </div>
-   </div>
-</div>  
+      <div class="col-auto mb-2">
+         <div class="d-flex">
+            <svg class="bi bi-gear-wide-connected" xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#ffffff" viewBox="0 0 16 16">
+            <path d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434l.071-.286zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5zm0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78h4.723zM5.048 3.967c-.03.021-.058.043-.087.065l.087-.065zm-.431.355A4.984 4.984 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8 4.617 4.322zm.344 7.646l.087.065-.087-.065z"></path>
+            </svg>
+            <p class="mb-0 text-light ms-2">Support teams across the world</p>
+         </div>
+      </div>
+      <div class="col-auto">
+         <div class="d-flex">
+            <svg class="bi bi-shield-lock-fill" xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="#ffffff" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm0 5a1.5 1.5 0 0 1 .5 2.915l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99A1.5 1.5 0 0 1 8 5z"></path>
+            </svg>
+            <p class="mb-0 text-light ms-2">Safe &amp; Secure online payment</p>
+         </div>
+      </div>
+      </div>
+      <div class="row flex-center">
+      <div class="col-auto py-4"><a href="#"><img class="img-fluid" src="assets/img/icons/f-logo.png" alt="" /></a></div>
+      </div>
+      <hr class="opacity-25" />
+      <div class="row justify-content-lg-around">
+      <div class="col-6 col-sm-4 col-lg-auto mb-3 order-0">
+         <h6 class="text-light lh-lg text-uppercase">Who we Are</h6>
+         <ul class="list-unstyled mb-md-4 mb-lg-0">
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">About</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Team</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Work With Us</a></li>
+         </ul>
+      </div>
+      <div class="col-6 col-sm-4 col-lg-auto mb-3 order-4 order-lg-1">
+         <h6 class="text-light lh-lg text-uppercase"> Woocommerce </h6>
+         <ul class="list-unstyled mb-md-4 mb-lg-0">
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Features</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Payments </a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Marketing</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Shipping</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Extension</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Store</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">eCommerce blog</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Development blog</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!"> Ideas board</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Mobile App</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Community</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Style Guide</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Email Newsletter</a></li>
+         </ul>
+      </div>
+      <div class="col-6 col-sm-4 col-lg-auto mb-3 order-1 order-lg-2">
+         <h6 class="text-light lh-lg text-uppercase">Other products </h6>
+         <ul class="list-unstyled mb-md-4 mb-lg-0">
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Storefront</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">WooSlider</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Sensei</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Sensei Extensions</a></li>
+         </ul>
+      </div>
+      <div class="col-6 col-sm-4 col-lg-auto mb-3 order-3 order-lg-3">
+         <h6 class="text-light lh-lg text-uppercase">Support</h6>
+         <ul class="list-unstyled mb-md-4 mb-lg-0">
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Documentation</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Customizations</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Support Policy</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Contact </a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">COVID-19 Resources</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Privacy Notice for </a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">California Users</a></li>
+         </ul>
+      </div>
+      <div class="col-6 col-sm-4 col-lg-auto mb-3 order-2 order-lg-4">
+         <h6 class="text-light lh-lg text-uppercase">We recommend</h6>
+         <ul class="list-unstyled mb-md-4 mb-lg-0">
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">WooExperts</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Hosting Solutions</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Pre-sales FAQ</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Success Stories</a></li>
+            <li class="lh-lg"><a class="text-light fs--1 text-decoration-none" href="#!">Design Feedback Group</a></li>
+         </ul>
+      </div>
+      </div>
+   </div><!-- end of .container-->
+
+</section>
+      <!-- <section> close ============================-->
+      <!-- ============================================-->
 <!-- end of footer -->
