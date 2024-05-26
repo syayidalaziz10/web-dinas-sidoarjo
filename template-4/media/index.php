5a95bfbc273990e24f63deb5a48ed7b290143145
syayidalaziz10 <shorashiro15@gmail.com>
2024-05-26 18:33:30 +0700

    [command prof_sty] change

--- template-4/media/index.php
+++ template-4/media/index.php
@@ -1,20 +1,20 @@
 <?php 
 
 
-require('../../conf/config.php');
-require('../../conf/phpFunction.php');
+  require('../../conf/config.php');
+  require('../../conf/phpFunction.php');
 
-  if(!empty(loadRecText('prof_sty', 'pub_profile', '_active=1'))){
-	$_urlTemp = explode('/', $_SERVER['REQUEST_URI']);
-	if(loadRecText('prof_sty', 'pub_profile', '_active=1') != $_urlTemp[1]){
-		echo '<meta content="0; url=http://'.$_SERVER['SERVER_NAME'].'/'.loadRecText('prof_sty', 'pub_profile', '_active=1').'" http-equiv="refresh">';	
-	}
-  } else {
-	echo '<meta content="0; url=http://'.$_SERVER['SERVER_NAME'].'/template-1" http-equiv="refresh">';	
-  }
+  // if(!empty(loadRecText('prof_sty', 'pub_profile', '_active=1'))){
+	// $_urlTemp = explode('/', $_SERVER['REQUEST_URI']);
+	// if(loadRecText('prof_sty', 'pub_profile', '_active=1') != $_urlTemp[1]){
+	// 	echo '<meta content="0; url=http://'.$_SERVER['SERVER_NAME'].'/'.loadRecText('prof_sty', 'pub_profile', '_active=1').'" http-equiv="refresh">';	
+	// }
+  // } else {
+	// echo '<meta content="0; url=http://'.$_SERVER['SERVER_NAME'].'/template-1" http-equiv="refresh">';	
+  // }
 
-$queryProfile    = $mysqli->query('SELECT * FROM pub_profile');
-$profileData     = $queryProfile->fetch_all(MYSQLI_ASSOC);
+  $queryProfile    = $mysqli->query('SELECT * FROM pub_profile');
+  $profileData     = $queryProfile->fetch_all(MYSQLI_ASSOC);
 
 
 ?>
